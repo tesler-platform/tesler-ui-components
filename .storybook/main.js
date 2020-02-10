45c6f521b8b87268627a8d7497b97d7b4a6056bf
kdergachev <dergachev@hotmail.com>
2020-02-10 21:53:28 +0300

    fix storybook configuration after CRA rejecting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,27 @@
+const path = require("path")
+
 module.exports = {
     stories: ['../src/**/*.stories.tsx'],
+    webpackFinal: async config => {
+        config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          include: path.resolve(__dirname, "../src"),
+          use: [
+              {
+                loader: require.resolve('ts-loader'),
+              },
+              {
+                  loader: require.resolve('react-docgen-typescript-loader'),
+                  options: {
+                    tsconfigPath: path.resolve(__dirname, "../tsconfig.json")
+                  }
+              }
+          ],
+        })
+        config.resolve.extensions.push('.ts', '.tsx');
+        return config
+    },
     addons: [
-        {
-            name: '@storybook/preset-create-react-app',
-            options: {
-                tsDocgenLoaderOptions: {},
-            },
-        },
         '@storybook/addon-docs'
     ]
 }
